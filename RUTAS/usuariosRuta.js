0f7283754b9937d07de77eeabad90d1a440b7f4b
Kevin <heroaltair2@gmail.com>
2022-06-02 19:20:45 -0500

    Se añadió la ruta para el inicio de sesión en el archivo usuariosRuta.js como /login

--- RUTAS/usuariosRuta.js
+++ RUTAS/usuariosRuta.js
@@ -6,4 +6,5 @@ var usuarioControl = require('../CONTROLADOR/usuariosControl')
 var api = express.Router();
 api.get('/probando-controlador', usuarioControl.prueba)
 api.post('/registro', usuarioControl.registrarUsuario)
+api.post('/login', usuarioControl.iniciarSesion)
 module.exports = api
