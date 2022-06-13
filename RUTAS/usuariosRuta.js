6c4e5ff37b13180025db7089ef2a9d58142266b8
Kevin <heroaltair2@gmail.com>
2022-06-13 08:31:59 -0500

    Se me había olvidado añadir la diagonal al inicio en  la ruta de 'bienvenido' jsjs, que pendejo

--- RUTAS/usuariosRuta.js
+++ RUTAS/usuariosRuta.js
@@ -5,7 +5,7 @@ var usuarioControl = require('../CONTROLADOR/usuariosControl')
 
 var api = express.Router();
 api.get('/probando-controlador', usuarioControl.probar)
-api.get('bienvenido', usuarioControl.darBienvenida)
+api.get('/bienvenido', usuarioControl.darBienvenida)
 api.post('/registro', usuarioControl.registrarUsuario)
 api.post('/login', usuarioControl.iniciarSesion)
 module.exports = api
