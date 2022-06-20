20ec91d0473a0f9626253f2fcbbad06b4b2ec692
Kevin <heroaltair2@gmail.com>
2022-06-20 09:23:43 -0500

    Se añadió la ruta para la función actualizarUsuario, la cual utiliza la funcion validarAcceso que hemos creado anteriormente

--- RUTAS/usuariosRuta.js
+++ RUTAS/usuariosRuta.js
@@ -2,6 +2,7 @@
 
 var express = require('express');
 var usuarioControl = require('../CONTROLADOR/usuariosControl')
+var md_auth = require('../MIDDLEWARE/autenticar')
 var api = express.Router();
 
 
@@ -9,4 +10,5 @@ api.get('/probando-controlador', usuarioControl.probar)
 api.get('/bienvenido', usuarioControl.darBienvenida)
 api.post('/registro', usuarioControl.registrarUsuario)
 api.post('/login', usuarioControl.iniciarSesion)
+api.put('/actualizar-usuario/:id', md_auth.validarAcceso, usuarioControl.actualizarUsuario)
 module.exports = api
